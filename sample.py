1f3995c583bd65fab3f082f3ec4569056a748522
Taylor Cooke <taylorcooke@gmail.com>
2019-08-11 19:53:04 -0400

    3d volcano

--- sample.py
+++ sample.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+df_v = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/volcano.csv")
 df = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/datasets/master/volcano_db.csv", encoding="iso-8859-1"
 )
@@ -15,19 +16,25 @@ values=df['Status'].value_counts()
 
 # fig = px.histogram(df, x="Country", color="Status").update_xaxes(categoryorder="total descending")
 # fig = px.bar(df, x='Volcano Name', y='Elev', color='Type', height=400)
-fig = go.Figure(data=go.Scattergeo(
-    lon = df['Longitude'],
-    lat = df['Latitude'],
-    mode = 'markers',
-    showlegend=False,
-    marker=dict(color="crimson", size=4, opacity=0.8)
-))
 
-fig.update_geos(
-    projection_type="orthographic",
-    landcolor="white",
-    oceancolor="MidnightBlue",
-    showocean=True,
-    lakecolor="LightBlue"
-)
+# Making a globe
+# fig = go.Figure(data=go.Scattergeo(
+#     lon = df['Longitude'],
+#     lat = df['Latitude'],
+#     mode = 'markers',
+#     showlegend=False,
+#     marker=dict(color="crimson", size=4, opacity=0.8)
+# ))
+
+# fig.update_geos(
+#     projection_type="orthographic",
+#     landcolor="white",
+#     oceancolor="MidnightBlue",
+#     showocean=True,
+#     lakecolor="LightBlue"
+# )
+fig = go.Figure(data=[go.Surface(z=df_v.values)])
+
+fig.update_layout(title='Volcano', autosize=False,width=500, height=500, margin=dict(l=65, r=50, b=65, t=90))
+
 fig.show()
