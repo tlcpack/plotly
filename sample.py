0169fadcc18127c3e94c68020e208e933d5cb3a6
Taylor Cooke <taylorcooke@gmail.com>
2019-08-11 11:15:41 -0400

    first histogram displays

---
+++ sample.py
@@ -0,0 +1,10 @@
+import numpy as np
+import plotly.express as px
+import pandas as pd
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/volcano_db.csv", encoding="iso-8859-1"
+)
+df.head()
+
+fig = px.histogram(df, x="Country")
+fig.show()
